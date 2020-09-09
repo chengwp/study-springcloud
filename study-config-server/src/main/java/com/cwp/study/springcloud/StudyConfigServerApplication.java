0cdfc24d327d9ee290350cd86ad4dd310e0bc126
chengwp <1623405873@qq.com>
2020-09-09 23:33:24 +0800

    增加配置中心

--- study-config-server/src/main/java/com/cwp/study/springcloud/StudyConfigServerApplication.java
+++ study-config-server/src/main/java/com/cwp/study/springcloud/StudyConfigServerApplication.java
@@ -3,15 +3,15 @@ package com.cwp.study.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-
-@EnableZuulProxy
 @SpringBootApplication
-public class StudyApiGatewayApplication {
+@EnableConfigServer
+@EnableDiscoveryClient
+public class StudyConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyApiGatewayApplication.class, args);
+        SpringApplication.run(StudyConfigServerApplication.class, args);
     }
 
 }
