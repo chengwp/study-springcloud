de3a313663642599434010288411614e30c6ddc3
chengwp <1623405873@qq.com>
2020-09-10 23:00:49 +0800

    增加断路器配置

--- study-consumer/src/main/java/com/cwp/study/springcloud/consumer/controller/ConsumerController.java
+++ study-consumer/src/main/java/com/cwp/study/springcloud/consumer/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.cwp.study.springcloud.consumer.controller;
 
 import com.cwp.study.springcloud.consumer.feign.FeignApiService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,18 @@ class ConsumerController {
     @Autowired
     private FeignApiService feignApiService;
 
+    @HystrixCommand(fallbackMethod = "helloError")
     @RequestMapping("/test")
     public Map hello(){
         HashMap hashMap=new HashMap();
         String value= feignApiService.hello();
         hashMap.put("key",value);
+        return hashMap;
+    }
+
+    public Map helloError() {
+        HashMap hashMap=new HashMap();
+        hashMap.put("key","error");
         return hashMap;
     }
 }
